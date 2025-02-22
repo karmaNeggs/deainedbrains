38bf8bc89c741eb72797f44ea898289ad6b671d1
karmaNeggs <55484527+karmaNeggs@users.noreply.github.com>
2025-02-22 08:54:33 +0530

    Update dockerfile.py

--- dockerfile.py
+++ dockerfile.py
@@ -1,6 +1,6 @@
 FROM python:3.9-slim
 
-# Install Tesseract and dependencies
+# Install Tesseract OCR and its dependencies
 RUN apt-get update && \
     apt-get install -y tesseract-ocr libtesseract-dev && \
     rm -rf /var/lib/apt/lists/*
