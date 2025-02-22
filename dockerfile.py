1b991926704581d2a23c20d1d44f7252cc0ca42e
karmaNeggs <55484527+karmaNeggs@users.noreply.github.com>
2025-02-22 08:30:28 +0530

    Update dockerfile.py
    Updated dockerfile to install tesseract

--- dockerfile.py
+++ dockerfile.py
@@ -1,18 +1,17 @@
-# Use an official Python runtime as a parent image
 FROM python:3.9-slim
 
-# Set the working directory in the container
+# Install Tesseract and dependencies
+RUN apt-get update && \
+    apt-get install -y tesseract-ocr libtesseract-dev && \
+    rm -rf /var/lib/apt/lists/*
+
 WORKDIR /app
 
-# Copy requirements file and install dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the current directory contents into the container at /app
 COPY . .
 
-# Expose the port that Streamlit uses
 EXPOSE 8501
 
-# Run the Streamlit app
 CMD ["streamlit", "run", "main.py", "--server.port", "8501", "--server.enableCORS", "false"]
